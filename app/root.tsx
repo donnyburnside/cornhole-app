e80167423e3ee7ba0d71660d3ca2961369143d78
Donny Burnside <donnyburnside1@gmail.com>
2022-08-31 23:55:11 +0100

    Updated the app title

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Cornhole App",
   viewport: "width=device-width,initial-scale=1",
 });
 
