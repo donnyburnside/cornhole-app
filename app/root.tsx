a81f976976e9e150db84a9124d933e5d5df3399a
Donny Burnside <donnyburnside1@gmail.com>
2022-09-01 01:55:20 +0100

    Updated body classes

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="font-sans antialiased bg-gray-50 text-gray-700">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
