1afbc2c5ead527631c3c6a6cc2ae4ac41b0cf3f7
Donny Burnside <donnyburnside1@gmail.com>
2022-09-03 00:00:38 +0100

    Updated Dashboard route component
    It now uses the newly added components

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,10 +2,13 @@ import {
   LoaderFunction
 } from "@remix-run/node";
 import {
-  Outlet
+  Outlet,
 } from "@remix-run/react";
 
 import { Layout } from "~/components/layout";
+import { Hero } from "~/components/hero";
+import { Leaderboard } from "~/components/leaderboard";
+import { Export } from "~/components/export";
 
 export const loader: LoaderFunction = async ({ request }) => {
   // Fake some loading
@@ -13,13 +16,21 @@ export const loader: LoaderFunction = async ({ request }) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   // Completed server-side tasks, returning nothing
-  return null
+  return null;
 }
 
 export default function Dashboard() {
   return (
     <Layout>
-      <h1>Dashboard</h1>
+      <main className="col-span-full sm:col-span-2 sm:self-start mb-10 sm:mb-0">
+        <Hero />
+      </main>
+
+      <aside className="col-span-full sm:col-span-1 sm:self-start">
+        <Leaderboard />
+        <Export />
+      </aside>
+
       <Outlet />
     </Layout>
   );
