e9c379e25a5afbb753080cfda911699978606fa4
Donny Burnside <donnyburnside1@gmail.com>
2022-09-06 08:56:49 +0100

    Updated Dashboard route component
    - Added a <Link> to the New Game route

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,6 +2,7 @@ import {
   LoaderFunction
 } from "@remix-run/node";
 import {
+  Link,
   Outlet,
 } from "@remix-run/react";
 
@@ -24,6 +25,9 @@ export default function Dashboard() {
     <Layout>
       <main className="col-span-full sm:col-span-2 sm:self-start mb-10 sm:mb-0">
         <Hero />
+        <Link className="inline-flex flex-row flex-nowrap justify-center items-center w-full sm:w-auto mt-2.5 px-6 py-3 text-sm leading-none text-center text-white bg-indigo-600" to="new">
+          New Game
+        </Link>
       </main>
 
       <aside className="col-span-full sm:col-span-1 sm:self-start">
