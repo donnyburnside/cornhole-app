5ca4767a515477d36f482df64f6f52b6eebb8b38
Donny Burnside <donnyburnside1@gmail.com>
2022-09-06 08:49:59 +0100

    Updated New route component
    - Added New Game Modal component
    - Added Action function for handling New Game form submissions
    - Parsed the form data, ready for using

--- app/routes/dashboard/new.tsx
+++ app/routes/dashboard/new.tsx
@@ -1,5 +1,50 @@
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+import { NewGameModal } from "~/components/new-game-modal";
+
+export const action: ActionFunction = async ({
+  request,
+}) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const updatedData = Object.entries(data).reduce((prev: any, [name, value]) => {
+    const re = new RegExp(/([^[]+(?=]))/g);
+    const name_indices = name.match(re)?.map(x => Number(x));
+
+    if (name_indices && name_indices.length) {
+      const round_index = Number(name_indices[0]);
+      const player_index = Number(name_indices[1]);
+
+      if (!prev.hasOwnProperty('rounds')) {
+        prev.rounds = new Array();
+      };
+      if (!prev.rounds[round_index]) {
+        prev.rounds[round_index] = new Array();
+      }
+
+      prev.rounds[round_index][player_index] = Number(value);
+    } else {
+      prev[name] = value;
+    }
+
+    return prev;
+  }, {});
+
+  console.log('Save Game:', updatedData);
+
+  return redirect("/dashboard");
+};
+
+const onCloseModal = () => {
+  if ( confirm("Are you sure you want to abandon this game? Any progress will be lost.") ) {
+    console.log('Close Modal');
+  }
+};
+
 export default function New() {
   return (
-    <p>Start a new game</p>
+    <NewGameModal onDismiss={onCloseModal} />
   );
 }
